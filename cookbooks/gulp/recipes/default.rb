db975a53beaf1e4839d9fe671183d4f571988e6b
Sergey Sergin <sergey-sergin@kruhak.name>
2015-07-15 15:42:11 +0600

    install gulp global and local

--- cookbooks/gulp/recipes/default.rb
+++ cookbooks/gulp/recipes/default.rb
@@ -4,6 +4,18 @@
 # Recipe:: default
 #
 
-nodejs_npm "install --global gulp"
-nodejs_npm "install --save-dev gulp"
-nodejs_npm "install gulp-autoprefixer  gulp-livereload gulp-csso gulp-imagemin gulp-uglify gulp-concat --save-dev"
+nodejs_npm "gulp"
+nodejs_npm "gulp-autoprefixer"
+nodejs_npm "gulp-livereload"
+nodejs_npm "gulp-csso"
+nodejs_npm "gulp-imagemin"
+nodejs_npm "gulp-uglify"
+nodejs_npm "gulp-concat"
+
+bash 'install_gulp_local'  do
+  code "su #{node['gulp']['user']} -l -c 'npm install --save-dev gulp'"
+end
+
+bash 'install_gulp_plugins'  do
+  code "su #{node['gulp']['user']} -l -c 'npm install gulp-autoprefixer  gulp-livereload gulp-csso gulp-imagemin gulp-uglify gulp-concat --save-dev'"
+end
